88b3039a317652f1a7089b56bcde2cf9daa44814
Rohit Joshi <rohit.joshi@joshsoftware.com>
2020-02-27 19:40:24 +0530

    Implement single method to accept string and match with specified regex type

--- 03_regex_email_ip_url.rb
+++ 03_regex_email_ip_url.rb
@@ -1,48 +1,37 @@
 # Write Regex for email phone_number IP URL
 # Read about each_with_index, map(), with_index, select, collect
-
-email = "rohit@joshi.com"
-mobile = "8554801616"
-
-if email.match(/^\w+@\w+\.com$/)
-  puts "valid email" 
-else 
-  puts "invalid email"
-end
-
-if mobile.match(/^\d{10}$/)
-  puts "valid mobile" 
-else 
-  puts "invalid mobile"
-end
-
-def ip_matcher(ip)
-  valid_part_count = 0
-  ip_array = ip.split(".")
-
-  ip_array.each do |part|
-    if (part.match(/^\d{1,3}$/) and part.to_i <= 255 and part.to_i >= 0)
-      valid_part_count += 1
-    else 
-      return "Invalid IP"
+def regex_matcher(string, type)
+  return false if string.nil?()
+  case type
+  when "email"
+    string.match(/^\w+@\w+\.com$/)
+  when "mobile"
+    string.match(/^\d{10}$/)
+  when "ip"
+    valid_part_count = 0
+    ip_array = string.split(".")
+    ip_array.each do |part|
+      if (part.match(/^\d{1,3}$/) and part.to_i <= 255 and part.to_i >= 0)
+        valid_part_count += 1
+      end
     end
-  end
-
-  if valid_part_count == 4
-    return "valid IP"
-  else
-    return "invalid IP"
-  end
-end
-
-puts ip_matcher("13.25.1.255")
-
-def url_matcher(url)
-  if url.match(/[(http)(https)]:\/\/(www.)?\w+\.\w+/)
-    puts "valid URL" 
-  else 
-    puts "invalid URL"
+    if valid_part_count == 4
+      return true
+    else
+      return false
+    end
+  when "URL"
+    string.match(/[(http)(https)]:\/\/(www.)?\w+\.\w+/)
   end
 end
 
-url_matcher("https://www.google.com")
+regex_matcher("rohit@joshi.com", "email")
+regex_matcher("8554801616", "mobile")
+regex_matcher("192.168.1.1", "ip")
+regex_matcher("https://www.google.com", "URL")
+
+if(regex_matcher("rohit@joshi.com", "email"))
+  puts true
+else
+  puts false
+end
